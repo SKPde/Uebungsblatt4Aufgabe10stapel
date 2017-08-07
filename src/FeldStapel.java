82ab4bd02dfbb5d43f56df63e3276bfaa06975f8
s.kuebel <s.kuebel@otterbach.de>
2017-08-07 14:12:54 +0200

    Feldstruktur Funktioniert auch. Zwei Nutzbare Hauptprogramme verfügbar: eines für verkettete Liste, ein anderes für die Liste in Feldstruktur

--- src/FeldStapel.java
+++ src/FeldStapel.java
@@ -4,6 +4,7 @@ public class FeldStapel {
 	private Bauklotz[] klotzarray = new Bauklotz[0];
 	private Bauklotz[] temparray = new Bauklotz[0];
 	private int lange = 0;
+	Bauklotz bauklotz;
 	
 	
 	public void push(Bauklotz klotz) {
@@ -16,12 +17,24 @@ public class FeldStapel {
 	}
 	
 	public Bauklotz pop() {
-		
-		
-		return null;
+		if(klotzarray.length > 0) {
+			bauklotz = klotzarray[Math.max(0, (klotzarray.length-1))];
+			klotzarray[Math.max(0, (klotzarray.length-1))] = null;
+			temparray = new Bauklotz[Math.max(0, (klotzarray.length-1))];
+			System.arraycopy(klotzarray, 0, temparray, 0, Math.max(0, (klotzarray.length-1)));
+			klotzarray = new Bauklotz[temparray.length];
+			System.arraycopy(temparray, 0, klotzarray, 0, temparray.length);
+			return bauklotz;
+		}else {
+			return null;
+		}
 	}
 	
 	public Bauklotz peek() {
-		return klotzarray[klotzarray.length-1];
+		if(klotzarray.length > 0) {
+			return klotzarray[Math.max(0, (klotzarray.length-1))];
+		}
+		
+		return null;
 	}
 }
--- src/Stapelhauptfeld.java
+++ src/Stapelhauptfeld.java
@@ -1,14 +1,49 @@
+import java.util.Scanner;
 
 public class Stapelhauptfeld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FeldStapel fs = new FeldStapel();
-		fs.push(new Bauklotz("Kübel"));
-		fs.push(new Bauklotz("Zoller"));
-		fs.push(new Bauklotz("Ernst"));
-		fs.push(new Bauklotz("Bean"));
-		System.out.println(fs.peek().toString());
+		Scanner in = new Scanner(System.in);
+		int aktion = 0;
+		String name;
+		Bauklotz klotz;
+
+	    do {
+	        System.out.println("Moegliche Aktionen:");
+	        System.out.println(" Klotz aufstapeln(1)," +
+	                           " Klotz runter nehmen (2)," + 
+	                           " Klotz anzeigen (3) " +
+	                           " Programmende (0)");
+	        System.out.print("Ihre Wahl: ");
+	        aktion = in.nextInt();
+	        if (aktion == 1) {
+	          System.out.print("Bauklotz aufstapeln:");
+	          System.out.print("Name: ");
+	          name = (String) in.next();
+	          System.out.println("                   ");
+	          fs.push(new Bauklotz(name));
+	        }
+	        else if (aktion == 2) {
+	          klotz = fs.pop();
+	          if (klotz == null)
+	            System.out.println("Keine Klötze mehr auf dem Stapel!");
+	          else {
+	            System.out.print("Es wurde folgender Klotz entnommen:  ");
+	            System.out.println(klotz);
+	          }
+	        }
+	        else if (aktion == 3) {
+	        		klotz = fs.peek();
+	        		if(klotz == null) {
+	        			System.out.println("Es befindet sich kein Klotz im Stapel");
+	        		} else {
+	        		System.out.print("Folgender Klotz liegt gerade oben:  ");
+	            System.out.println(klotz);
+	        		}
+	        }
+	      } while (aktion != 0);
 	}
 
 }
