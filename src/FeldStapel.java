2d7586fd33ed9853134e0fc8839bd43229aa2122
s.kuebel <s.kuebel@otterbach.de>
2017-08-07 13:44:52 +0200

    Feldstrukturstapel nimmt nun Objekte auf. Auch das Peeken von Objekten ist möglich.

--- src/FeldStapel.java
+++ src/FeldStapel.java
@@ -1,4 +1,27 @@
 
 public class FeldStapel {
 
+	private Bauklotz[] klotzarray = new Bauklotz[0];
+	private Bauklotz[] temparray = new Bauklotz[0];
+	private int lange = 0;
+	
+	
+	public void push(Bauklotz klotz) {
+		lange = klotzarray.length + 1;
+		temparray = new Bauklotz[lange];
+		System.arraycopy(klotzarray, 0, temparray, 0, klotzarray.length);
+		temparray[lange-1] = klotz;
+		klotzarray = new Bauklotz[temparray.length];
+		System.arraycopy(temparray, 0, klotzarray, 0, temparray.length);
+	}
+	
+	public Bauklotz pop() {
+		
+		
+		return null;
+	}
+	
+	public Bauklotz peek() {
+		return klotzarray[klotzarray.length-1];
+	}
 }
--- src/Stapelhauptfeld.java
+++ src/Stapelhauptfeld.java
@@ -3,7 +3,12 @@ public class Stapelhauptfeld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FeldStapel fs = new FeldStapel();
+		fs.push(new Bauklotz("Kübel"));
+		fs.push(new Bauklotz("Zoller"));
+		fs.push(new Bauklotz("Ernst"));
+		fs.push(new Bauklotz("Bean"));
+		System.out.println(fs.peek().toString());
 	}
 
 }
