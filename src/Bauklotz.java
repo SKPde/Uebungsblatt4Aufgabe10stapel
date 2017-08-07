f98864fa46cbdf024ce17f65d45338c4202746bf
s.kuebel <s.kuebel@otterbach.de>
2017-08-07 11:35:45 +0200

    Bauklotz als Stapelprodukt ergänzt

---
+++ src/Bauklotz.java
@@ -0,0 +1,14 @@
+
+public class Bauklotz {
+
+	private String name;
+	
+	public Bauklotz(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+	
+}
--- src/KettenStapel.java
+++ src/KettenStapel.java
@@ -1,4 +1,22 @@
 
 public class KettenStapel {
-
+	
+	
+	
+	
+	public void push() {
+		
+	}
+	
+	public String pop() {
+		
+		
+		return "test";
+	}
+	
+	public String peek() {
+		
+		
+		return "peektest";
+	}
 }
